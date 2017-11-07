ec2faad09b62a5358922aefa4dcd51f3d2b79b53
Kevin Idzik (RIT Student) <kdi4293@ad.rit.edu>
2017-11-07 14:16:06 -0500

    Beginning tokens

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
+const csrf = require('csurf'); // Prevent Cross-Site-Request-Forgery
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000; // Node server connection
 
@@ -42,6 +43,7 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by'); // Disable this so others don't know what framework we're running
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -54,11 +56,25 @@ app.use(session({
   secret: 'Domo Arigato', // Seed for hashing/unique session keys
   resave: true, // Refresh the key to keep it active
   saveUninitialized: true, // Always make sessions even when not logged in
+  cookie: {
+    httpOnly: true, // Prevent client side JavaScript from accessing cookies
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views/`);
 app.use(cookieParser());
+
+// Don't respond to users with a bad token
+// They're up to nonsense
+app.use(csrf);
+app.use((err, req, res, next) => {
+  // If this isn't tomfoolery
+  if (err.code !== 'EBADCSFTOKEN') { return next(err); }
+
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
